1c9e70c4b7e3ccd49c93403a365772ba2a057a48
K.Kimura <dotnsf@gmail.com>
2017-05-17 17:34:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# node-canvas
+
+## Official URL
+
+https://github.com/Automattic/node-canvas
+
+## Install ( on Ubuntu )
+
+- (Install node.js and npm)
+
+- $ sudo apt-get update
+
+- $ sudo apt-get install libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev build-essential g++
+
+- $ npm install canvas
+
+
---
+++ app.js
@@ -0,0 +1,43 @@
+//. app.js
+
+var express = require( 'express' );
+var app = express();
+
+var fs = require( 'fs' );
+
+var Canvas = require( 'canvas' ),
+    Image = Canvas.Image;
+
+var cfenv = require( 'cfenv' );
+var appEnv = cfenv.getAppEnv();
+
+app.use( express.static( __dirname + '/public' ) );
+
+app.get( '/generate_image', function( req, res ){
+  if( req.query.filename ){
+    var filename = req.query.filename;
+    var img = new Image;
+    var canvas = new Canvas( 300, 300 );
+    var ctx = canvas.getContext( '2d' );
+
+    ctx.beginPath();
+    ctx.moveTo( 100, 100 );
+    ctx.lineTo( 200, 200 );
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+
+    var b64 = canvas.toDataURL().split( ',' )[1];
+    var buf = new Buffer( b64, 'base64' );
+    fs.writeFile( __dirname + '/public/' + filename, buf, function(){
+      res.redirect( '/' + filename );
+    });
+  }else{
+    res.write( 'Specify filename=xxxx' );
+    res.end();
+  }
+});
+
+app.listen( appEnv.port );
+console.log( "server is running on " + appEnv.port + " ..." );
+
+
